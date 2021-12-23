7dd2d6ef9a26fc33d66f4d3600a65153c7b055e3
Weihang Li <weihang.li@tum.de>
2021-12-23 21:32:32 +0100

    update

--- 3.Lie_Group/draw_trajectory.cpp
+++ 3.Lie_Group/draw_trajectory.cpp
@@ -2,45 +2,87 @@
 #include <string>
 #include <iostream>
 #include <fstream>
-#include <Eigen/StdVector>
+#include <Eigen/Core>
+#include <unistd.h>
 // need pangolin for plotting trajectory
 #include <pangolin/pangolin.h>
 
 using namespace std;
 
 // path to trajectory file
-string trajectory_file = "./trajectory.txt";
+string trajectory_file = "../trajectory.txt";
+string groundtruth_file = "../groundtruth.txt";
+string estimated_file = "../estimated.txt";
 
-// function for plotting trajectory, don't edit this code
+typedef vector<Sophus::SE3d, Eigen::aligned_allocator<Sophus::SE3d>> TrajectoryType;
+
 // start point is red and end point is blue
-void DrawTrajectory(vector<Sophus::SE3, Eigen::aligned_allocator<Sophus::SE3>>);
+void DrawTrajectory(TrajectoryType);
+void DrawTwoTrajectory(TrajectoryType, TrajectoryType);
+void CalculateError(TrajectoryType, TrajectoryType );
 
 int main(int argc, char **argv) {
 
-    vector<Sophus::SE3, Eigen::aligned_allocator<Sophus::SE3>> poses;
+    TrajectoryType poses;
+    TrajectoryType groundtruth;
+    TrajectoryType estimated;
 
     /// implement pose reading code
-    // start your code here (5~10 lines)
-    ifstream fin(trajectory_file);
-    for ( int i=0; i<620; i++ )
-    {
-        double data[8] = {0};
-        for ( auto& d:data )
-            fin>>d;
-        Eigen::Quaterniond q( data[7], data[4], data[5], data[6] );
-        Eigen::Vector3d t(data[1], data[2], data[3]);
-        Sophus::SE3 SE3_qt(q,t);
-        poses.push_back(SE3_qt);
-    }
-    // end your code here
+    std::ifstream file_reader(trajectory_file);
+    std::ifstream groundtruth_reader(groundtruth_file);
+    std::ifstream estimated_reader(estimated_file);
+    double time, tx, ty, tz, qx, qy, qz, qw;
 
-    // draw trajectory in pangolin
-    DrawTrajectory(poses);
+    // Method 1
+    // while (!file_reader.eof()){
+    //     file_reader >> time >> tx >> ty >> tz >> qx >> qy >> qz >> qw;
+    //     Eigen::Vector3d t(tx, ty, tz);
+    //     Eigen::Quaterniond q(qw, qx, qy, qz);
+    //     q.normalize();
+    //     Eigen::Matrix3d Rotation_Matrix(q);
+    //     Sophus::SE3d SE3_from_Eigen(Rotation_Matrix, t);
+    //     poses.push_back(SE3_from_Eigen);
+    // }
+    // cout << "There are " << poses.size() << " timestamps." << endl;
+
+    // Method 2
+    while(file_reader >> time >> tx >> ty >> tz >> qx >> qy >> qz >> qw){
+        poses.push_back(Sophus::SE3d(Eigen::Quaterniond(qw, qx, qy, qz), Eigen::Vector3d(tx, ty, tz)));
+    }
+    cout << "There are " << poses.size() << " timestamps." << endl;
+
+    //    DrawTrajectory(poses);
+
+    while(groundtruth_reader >> time >> tx >> ty >> tz >> qx >> qy >> qz >> qw){
+        groundtruth.push_back(Sophus::SE3d(Eigen::Quaterniond(qw, qx, qy, qz), Eigen::Vector3d(tx, ty, tz)));
+    }
+    cout << "There are " << groundtruth.size() << " timestamps." << endl;
+
+    while(estimated_reader >> time >> tx >> ty >> tz >> qx >> qy >> qz >> qw){
+        estimated.push_back(Sophus::SE3d(Eigen::Quaterniond(qw, qx, qy, qz), Eigen::Vector3d(tx, ty, tz)));
+    }
+    cout << "There are " << estimated.size() << " timestamps." << endl;
+    DrawTwoTrajectory(groundtruth, estimated);
+
+    CalculateError(groundtruth, estimated);
+
     return 0;
 }
 
+void CalculateError(TrajectoryType groundtruth, TrajectoryType estimated){
+    double error(0.0);
+    double rmse(0.0);
+    for (size_t i = 0; i < groundtruth.size(); i++){
+        error = (groundtruth[i].inverse() * estimated[i]).log().norm();
+        rmse += error * error;
+    }
+    rmse = sqrt(rmse / groundtruth.size());
+    cout << "rmse: " << rmse << endl;
+}
+
 /*******************************************************************************************/
-void DrawTrajectory(vector<Sophus::SE3, Eigen::aligned_allocator<Sophus::SE3>> poses) {
+void DrawTrajectory(TrajectoryType poses) {
+
     if (poses.empty()) {
         cerr << "Trajectory is empty!" << endl;
         return;
@@ -81,4 +123,53 @@ void DrawTrajectory(vector<Sophus::SE3, Eigen::aligned_allocator<Sophus::SE3>> p
         usleep(5000);   // sleep 5 ms
     }
 
+}
+
+void DrawTwoTrajectory(TrajectoryType poses1, TrajectoryType poses2) {
+    if (poses1.empty() || poses2.empty()) {
+        cerr << "Trajectory is empty!" << endl;
+        return;
+    }
+
+    // create pangolin window and plot the trajectory
+    pangolin::CreateWindowAndBind("Groundtruth: red, Estimated: blue", 1024, 768);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    pangolin::OpenGlRenderState s_cam(
+            pangolin::ProjectionMatrix(1024, 768, 500, 500, 512, 389, 0.1, 1000),
+            pangolin::ModelViewLookAt(0, -0.1, -1.8, 0, 0, 0, 0.0, -1.0, 0.0)
+    );
+
+    pangolin::View &d_cam = pangolin::CreateDisplay()
+            .SetBounds(0.0, 1.0, pangolin::Attach::Pix(175), 1.0, -1024.0f / 768.0f)
+            .SetHandler(new pangolin::Handler3D(s_cam));
+
+
+    while (pangolin::ShouldQuit() == false) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        d_cam.Activate(s_cam);
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+        glLineWidth(2);
+        for (size_t i = 0; i < poses1.size() - 1; i++) {
+            glColor3f(1.0f, 0.0f, 0.0f);
+            glBegin(GL_LINES);
+            auto p1 = poses1[i], p2 = poses1[i + 1];
+            glVertex3d(p1.translation()[0], p1.translation()[1], p1.translation()[2]);
+            glVertex3d(p2.translation()[0], p2.translation()[1], p2.translation()[2]);
+            glEnd();
+
+            glColor3f(0.0f, 0.0f, 1.0f);
+            glBegin(GL_LINES);
+            auto p3 = poses2[i], p4 = poses2[i + 1];
+            glVertex3d(p3.translation()[0], p3.translation()[1], p3.translation()[2]);
+            glVertex3d(p4.translation()[0], p4.translation()[1], p4.translation()[2]);
+            glEnd();
+        }
+        pangolin::FinishFrame();
+        usleep(5000);   // sleep 5 ms
+    }
 }
